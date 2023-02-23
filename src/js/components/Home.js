418bd1d31c0d5af045583dd454f27db665722397
Olena Kurilets <elenakurilets@gmail.com>
2023-02-23 20:46:37 +0100

    Recent changes

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -10,10 +10,29 @@ class Home {
     const thisHome = this;
     thisHome.dom = {};
     thisHome.dom.wrapper = wrapper;
-
     const generatedHTML = templates.homeWidget();
     const element = utils.createDOMFromHTML(generatedHTML);
     thisHome.dom.wrapper.appendChild(element);
   }
 }
+
+function initPage() {
+  const order = document.querySelector('a[href="#order"]');
+  console.log('order', order);
+  const linkBooking = document.querySelector('.link_order');
+
+  order.addEventListener('click', function () {
+    order.classList.toggle('active');
+  });
+
+  const booking = document.querySelector('a[href="#booking"]');
+  console.log('booking', booking);
+
+  booking.addEventListener('click', function () {
+    order.classList.toggle('active');
+  });
+}
+
+initPage();
+
 export default Home;
